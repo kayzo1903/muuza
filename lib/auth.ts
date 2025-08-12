bc00f680080e0620ce19def8c356f17a7e333696
kayzo1903 <kaykinangukammari@gmail.com>
2025-08-12 16:28:37 +0300

    redirect_url change

--- lib/auth.ts
+++ lib/auth.ts
@@ -21,7 +21,7 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      redirectUri: "https://muuza.vercel.app/api/auth/callback/google",
+      redirectUri: `${process.env.NEXT_PUBLIC_SITE_URL}/api/auth/callback/google`,
     },
   },
 });
