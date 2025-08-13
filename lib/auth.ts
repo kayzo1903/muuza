50937ba6d40a8782e1086bdd24bac9db1bcb63bd
kayzo1903 <kaykinangukammari@gmail.com>
2025-08-13 04:11:06 +0300

    callback url added

--- lib/auth.ts
+++ lib/auth.ts
@@ -21,10 +21,12 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      redirectUri: `${process.env.NEXT_PUBLIC_BETTER_AUTH_URL}/api/auth/callback/google`
     },
     facebook: {
       clientId: process.env.FACEBOOK_CLIENT_ID as string,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+      redirectUri: `${process.env.NEXT_PUBLIC_BETTER_AUTH_URL}/api/auth/callback/facebook`
     }
   },
 });
